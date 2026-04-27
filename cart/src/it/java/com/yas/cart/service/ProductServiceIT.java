90f129abe0a4ef9c38df0fb058c0f6d586dadabf
Quoc Ky <quockinguyen.lv@gmail.com>
2026-04-27 18:40:03 +0200

    fix cart service 4

--- cart/src/it/java/com/yas/cart/service/ProductServiceIT.java
+++ cart/src/it/java/com/yas/cart/service/ProductServiceIT.java
@@ -12,7 +12,6 @@ import java.util.List;
 import com.yas.commonlibrary.IntegrationTestConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
