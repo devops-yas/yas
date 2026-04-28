29ec34c31ca749a192cfaacddafed5bfad8033a8
Quoc Ky <quockinguyen.lv@gmail.com>
2026-04-28 05:29:13 +0200

    fix 01

--- common-library/src/test/java/com/yas/commonlibrary/IntegrationTestConfiguration.java
+++ common-library/src/test/java/com/yas/commonlibrary/IntegrationTestConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.time.Duration;
 
 @TestConfiguration
 public class IntegrationTestConfiguration {
@@ -14,28 +17,40 @@ public class IntegrationTestConfiguration {
     @ServiceConnection
     public PostgreSQLContainer<?> postgresContainer() {
         return new PostgreSQLContainer<>("postgres:16")
-            .withReuse(true);
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test")
+            .withReuse(false) // TẮT REUSE ĐỂ TRÁNH RÁC TRÊN JENKINS
+            .waitingFor(Wait.forListeningPort())
+            .withStartupTimeout(Duration.ofMinutes(2));
     }
 
     @Bean(destroyMethod = "stop")
     public KeycloakContainer keycloakContainer() {
-        return new KeycloakContainer()
+        return new KeycloakContainer("quay.io/keycloak/keycloak:26.0")
             .withRealmImportFiles("/test-realm.json")
-            .withReuse(true);
+            .withReuse(false) // TẮT REUSE
+            .withStartupTimeout(Duration.ofMinutes(3));
     }
 
     @Bean
-    public DynamicPropertyRegistrar keycloakDynamicProperties(KeycloakContainer keycloakContainer) {
+    public DynamicPropertyRegistrar integrationTestProperties(
+            PostgreSQLContainer<?> postgresContainer, 
+            KeycloakContainer keycloakContainer) {
         return registry -> {
-            registry.add(
-                "spring.security.oauth2.resourceserver.jwt.issuer-uri",
-                () -> keycloakContainer.getAuthServerUrl() + "/realms/quarkus"
-            );
-            registry.add(
-                "spring.security.oauth2.resourceserver.jwt.jwk-set-uri",
-                () -> keycloakContainer.getAuthServerUrl()
-                    + "/realms/quarkus/protocol/openid-connect/certs"
-            );
+            // DATABASE PROPERTIES - CHỐT HẠ LỖI DIALECT
+            registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+            registry.add("spring.datasource.username", postgresContainer::getUsername);
+            registry.add("spring.datasource.password", postgresContainer::getPassword);
+            registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.PostgreSQLDialect");
+            registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.PostgreSQLDialect");
+            registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+
+            // KEYCLOAK PROPERTIES
+            registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                () -> keycloakContainer.getAuthServerUrl() + "/realms/quarkus");
+            registry.add("spring.security.oauth2.resourceserver.jwt.jwk-set-uri",
+                () -> keycloakContainer.getAuthServerUrl() + "/realms/quarkus/protocol/openid-connect/certs");
         };
     }
 }
