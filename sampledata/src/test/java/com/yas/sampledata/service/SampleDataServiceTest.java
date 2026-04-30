00f69731e340487b714eded4a2ee342738b9cd4a
hungnguyendz9913 <82220732+hungnguyendz9913@users.noreply.github.com>
2026-04-30 18:31:17 +0000

    Add unit tests for sampledata SampleDataService

---
+++ sampledata/src/test/java/com/yas/sampledata/service/SampleDataServiceTest.java
@@ -0,0 +1,73 @@
+package com.yas.sampledata.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.yas.sampledata.viewmodel.SampleDataVm;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+
+class SampleDataServiceTest {
+
+    @Test
+    void shouldReturnSuccessMessageEvenWhenScriptsFail() {
+        DataSource productDataSource = new ThrowingDataSource();
+        DataSource mediaDataSource = new ThrowingDataSource();
+        SampleDataService service = new SampleDataService(productDataSource, mediaDataSource);
+
+        SampleDataVm result = service.createSampleData();
+
+        assertEquals("Insert Sample Data successfully!", result.message());
+    }
+
+    private static final class ThrowingDataSource implements DataSource {
+        @Override
+        public Connection getConnection() throws SQLException {
+            throw new SQLException("Connection refused");
+        }
+
+        @Override
+        public Connection getConnection(String username, String password) throws SQLException {
+            throw new SQLException("Connection refused");
+        }
+
+        @Override
+        public PrintWriter getLogWriter() throws SQLException {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public void setLogWriter(PrintWriter out) throws SQLException {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public void setLoginTimeout(int seconds) throws SQLException {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public int getLoginTimeout() throws SQLException {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+            throw new SQLFeatureNotSupportedException("Not supported");
+        }
+
+        @Override
+        public <T> T unwrap(Class<T> iface) throws SQLException {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public boolean isWrapperFor(Class<?> iface) throws SQLException {
+            return false;
+        }
+    }
+}
